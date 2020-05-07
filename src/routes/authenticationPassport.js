8b14cc56388afe474a69552e11e3949168f12652
Ylenia <ylenia.d.23@gmail.com>
2020-05-07 19:42:14 +0000

    small improvements in html, signin passport (serialize error)

--- src/routes/authenticationPassport.js
+++ src/routes/authenticationPassport.js
@@ -4,9 +4,9 @@ const PASSPORT = require('../../lib/passport');
 
 const IS_LOGGED=(req, res, next)=>{
     if(req.session.loggedin){
-        return next();
-    }else{
         res.redirect('/list');
+    }else{
+        return next();
     }
 };
 
@@ -18,7 +18,7 @@ ROUTER.get('/signup', IS_LOGGED, (req, res)=>{
 });
 
 ROUTER.post('/signup', PASSPORT.authenticate('signupLocal',{ 
-    successRedirect:'/list',
+    successRedirect:'/signin',
     failureRedirect: '/signup',
     failureFlash: true
 }));
@@ -26,36 +26,23 @@ ROUTER.post('/signup', PASSPORT.authenticate('signupLocal',{
 /**
  * SIGN IN
  */
-/*ROUTER.get('/signin', IS_LOGGED, (req, res)=>{
+ROUTER.get('/signin', IS_LOGGED, (req, res)=>{
     res.render(`${req.app.get('PATH_AUTH')}/signin`);
 });
 
-ROUTER.post('/signin', IS_LOGGED, async(req, res)=>{
-    const {username, password}=req.body;
-    const QUERY = `select password from users where username='${username}'`;
-    const ENCRYPT_PASSWORD=await POOL.query(QUERY);  //devuelve un array con las contraseñas
-
-    if(ENCRYPT_PASSWORD.length>0){
-        const IS_VALID = await ENCRYPT.comparePassword(password, ENCRYPT_PASSWORD[0].password);
-        if(IS_VALID){
-            req.session.loggedin=true;
-            req.session.username=username;
-            res.redirect('/list');
-        }else{
-            res.redirect('/signin');
-        }
-    }else{
-        res.redirect('/signin');
-    }
-});*/
+ROUTER.post('/signin', PASSPORT.authenticate('signinLocal',{
+    successRedirect:'/list',
+    failureRedirect: '/signin',
+    failureFlash: true
+}));
 
 /**
  * SIGN OUT
  */
-/*ROUTER.get('/signout', async(req, res)=>{
+ROUTER.get('/signout', async(req, res)=>{
     req.session.logout;
     req.session.loggedin=false;
     res.redirect('/signin');
-});*/
+});
 
 module.exports=ROUTER;
