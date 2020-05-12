9f9b1dcc188d9e071548e98417d9c6d4bd5959b6
Ylenia <ylenia.d.23@gmail.com>
2020-05-12 13:41:27 +0000

    finish test sequelize

--- db-sequelize/db-pool.js
+++ db-sequelize/db-pool.js
@@ -54,20 +54,89 @@ sequelize.sync({force: false})
     });
 
 /**
- * SELECT * USER
+ * SELECT * USER //LIST
  */
-/*User.findAll().then(users => {
+/*User.findAll({attributes: [username]}).then(users => {
     let usersString = JSON.stringify(users, null, 4);
     let usersObject = JSON.parse(usersString);
     console.log(usersObject);
     //console.log(`ALL USERS: ${JSON.stringify(users, null, 4)}`);ç
 });*/
 
-User.findAll().then(users => {
+/*User.findAll().then(users => {
     let usersString = JSON.stringify(users, null, 4);
     let usersObject = JSON.parse(usersString);
     //SELECT *
     console.log(`ALL USERNAMES: ${usersObject.map((user)=>user.username)}`);
     //WHERE
     console.log(`USER KKK: ${usersObject.filter((user)=>user.username=='kkk')}`);
-});
+});*/
+
+/**
+ * ADD 
+ */
+
+/*User.create({username: 'Juan', password: '1234', fullname: 'Juan Pérez'})
+    .then(insertedUser=>{
+        console.log(`ID: ${insertedUser.id}`);
+    });*/
+
+/**
+ * EDIT
+ */
+    //GET
+/*User.findAll({
+    where: {
+        id: 30
+    }
+}).then(users=>{
+    //console.log(user[0].dataValues);
+    if(users.length>0){
+        let usersString = JSON.stringify(users, null, 4);
+        let usersObject = JSON.parse(usersString);
+        console.log(usersObject);
+    }else{
+        console.log('No hay ningun usuario con ese ID');
+    }
+    
+});*/
+
+//POST
+/*User.update({
+        username: 'Jon',
+        password: '1234',
+        fullname: 'Jon Calvo'
+    },{
+        where: {
+            id: 30
+        }
+    }
+).then(users=>console.log('UPDATE OK'));*/
+
+/**
+ * DELETE
+ */
+/*User.destroy({
+        where:{
+            id: 30
+        }
+    }
+).then(()=>console.log('DELETE OK'))
+.catch(err=>console.log(`ERROR DELETE: ${err.message}`));*/
+
+/**
+ * AUTH
+ */
+//POST SIGN UP
+/*async function findUser(){
+    const users = await sequelize.query(`select username from users where username="pepe"`, {type: sequelize.QueryTypes.SELECT});
+    console.log(users);
+}
+findUser();*/
+
+/*async function insertUser(){
+    const query=`insert into users (fullname, username, password) values ('Pepito Grillo', 'Pepito', '1234')`;
+    const users = await sequelize.query(query, {type: sequelize.QueryTypes.INSERT});
+    console.log('INSERT OK');
+}
+insertUser();*/
